684a4751a17d330055206eb37444033b87e88fa3
Jay Kim <jkim506@aucklanduni.ac.nz>
2016-07-29 17:59:58 +1200

    Different canvases

--- documentation/client/components/DemoPage.jsx
+++ documentation/client/components/DemoPage.jsx
@@ -1,33 +1,26 @@
 import React, { Component } from 'react';
 import RecognitionCanvas from 'recognition-canvas';
 
+import UndoRedoCanvas from './different_canvases/UndoRedoCanvas.jsx'
+import NormalDrawingCanvas from './different_canvases/NormalDrawingCanvas.jsx'
+import ColorCanvas from './different_canvases/ColorCanvas.jsx'
+import RecognitionOnlyCanvas from './different_canvases/RecognitionOnlyCanvas.jsx'
+import BeautificationCanvas from './different_canvases/BeautificationCanvas.jsx'
+
 export default class DemoPage extends React.Component {
 	
 	constructor(){
 		super();
-		this.test = this.test.bind(this);
-		this.alg = "$p";
-	}
-
-	test(){
-		console.log("test");
 	}
 
 	render(){
 		return(
 				<div>
-					<RecognitionCanvas recognitionAlgorithm={this.alg} 
-					recognitionTime={40} 
-					recognitionListener={this.test}
-					undoListener={this.test} 
-					redoListener={this.test} 
-					clearCanvasListener={this.test}
-					width="100%"
-					height="100%"
-					beautification={true}
-					disabledGestures={this.test}
-					enabledGestures={this.test}
-					addGestures={new Array()}/>
+					<UndoRedoCanvas />
+					<NormalDrawingCanvas />
+					<ColorCanvas />
+					<RecognitionOnlyCanvas />
+					<BeautificationCanvas />
 				</div>
 			);
 		
