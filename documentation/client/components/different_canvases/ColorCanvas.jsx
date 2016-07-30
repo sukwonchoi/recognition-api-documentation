ef65c83c70ef9126dcfe64947e12695876e02ec9
Sukwon Choi <kswc95@gmail.com>
2016-07-30 17:38:16 +1200

    recognitino + undo kinda done

--- documentation/client/components/different_canvases/ColorCanvas.jsx
+++ documentation/client/components/different_canvases/ColorCanvas.jsx
@@ -1,23 +1,66 @@
 import React, { Component } from 'react';
 import RecognitionCanvas from 'recognition-canvas';
+import { ChromePicker } from 'react-color'
+
 
 export default class ColorCanvas extends React.Component {
 	
+	
+
 	constructor(){
 		super();
+
+		this.state = {
+      displayColorPicker: false,
+      color: "#000000",
+  };
+
 		this.test = this.test.bind(this);
 		this.alg = "$p";
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+		this.handleClose = this.handleClose.bind(this);
 	}
 
 	test(){
 		console.log("test");
 	}
 
+	handleChange(color){
+
+		this.setState({ 
+			color: color.hex,
+			displayColorPicker: false
+		});
+	}
+
+	handleClick() {    
+		console.log("Handle close triggered");
+		this.setState({ displayColorPicker: !this.state.displayColorPicker });
+	};
+
+	handleClose() {
+		this.setState({ displayColorPicker: false });
+	};
+
 	render(){
 
 		const divStyle = {
-	      border: '2px solid black',
+	      border: '1px solid black',
     	};
+
+    const popover = {
+      position: 'absolute',
+      zIndex: '2',
+    }
+    const cover = {
+      position: 'fixed',
+      top: '0',
+      right: '0',
+      bottom: '0',
+      left: '0',
+    }
 
 
 		return(
@@ -31,14 +74,24 @@ export default class ColorCanvas extends React.Component {
 							clearCanvasListener={this.test}
 							width= {screen.width * 0.8}
 							height= {screen.height * 0.5}
+							color={this.state.color}
 							beautification={false}
 							disabledGestures={[""]}
 							enabledGestures={[""]}
 						/>
+						<button onClick={ this.handleClick }> Pick Color</button>
+						{ this.state.displayColorPicker ? <div style={ popover }>
+
+							<div style={ cover } onClick={ this.handleClose }/>
+							<ChromePicker onChangeComplete={ this.handleChange }/>
+						</div> : null }
+
 					</div>
 				</div>
 			);
 		
 	}
 }
+
+
 
